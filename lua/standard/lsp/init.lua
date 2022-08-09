c8f8e872f130e92949f1813f4c29535e063209a6
Sami EL KATEB <sami.elkateb@gmail.com>
2022-08-09 13:20:56 +0200

    fix: missing paths

--- lua/standard/lsp/init.lua
+++ lua/standard/lsp/init.lua
@@ -4,5 +4,5 @@ if not status_ok then
 	return
 end
 
-require("user.lsp.lsp-installer")
-require("user.lsp.handlers").setup()
+require("standard.lsp.lsp-installer")
+require("standard.lsp.handlers").setup()
